6d69279ab2c61a21be0894a377911745b031196c
Riya Dey <1805013@ugrad.cse.buet.ac.bd>
2023-08-08 02:27:32 +0600

    "Dockerfile added-2"

--- Backend/src/main/java/com/example/demo_1/Demo1Application.java
+++ Backend/src/main/java/com/example/demo_1/Demo1Application.java
@@ -2,14 +2,22 @@ package com.example.demo_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo1Application {
 
 	public static void main(String[] args) {
 
 
 		SpringApplication.run(Demo1Application.class, args);
+	}
+	@GetMapping("/api/public/count-users")
+	public String get()
+	{
+		return "hello";
 	}
 
 }
